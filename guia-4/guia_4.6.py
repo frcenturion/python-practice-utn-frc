884348c29b71ad2c93ca0fbf6c1ee66f4ea77b39
Franco Centurion <frcenturion@frba.utn.edu.ar>
2023-02-03 12:11:26 -0300

    Ejercicio 4.6 completo

---
+++ guia-4/guia_4.6.py
@@ -0,0 +1,35 @@
+# 4.6 - TARJETA DE BINGO
+
+# Realizar un programa que genere 15 numero aleatorios enteros en el rango del 1 al 100, 
+# que representaria la tarjeta de bingo de una persona. Una vez generado los numeros aleatorios solicitar al
+# usuario que ingrese 3 numeros enteros, a parƟr de alli mostrar los siguientes mensajes:
+# Si el usuario no marco ninguno de los numeros indicarlo diciendo “El jugador Ɵene mala suerte,
+# no marco ninguna casilla”.
+# Caso contrario mostrar “El jugador marco algun numero de la tarjeta”
+
+
+import random
+
+tarjeta_bingo = []
+
+for i in range(0,15):
+    tarjeta_bingo.append(random.randint(1,100))    
+
+
+print(tarjeta_bingo)
+
+numeros_jugador = []
+
+for i in range(0,3):
+    numeros_jugador.append(int(input('Ingrese un numero: ')))
+    
+
+variable = any(i in numeros_jugador for i in tarjeta_bingo)             # TENGO DUDAS ACERCA DE ESTO
+
+
+print(variable)
+
+    
+
+
+
